85ec21d0e3b7ccfd26703ee1bf51619f1a228d76
Odin <odinkl@stud.ntnu.no>
2023-10-25 19:05:01 +0200

    stjerne-anmeldelse

--- munch.js
+++ munch.js
@@ -37,3 +37,18 @@ function navbarOpacity(){
 
 setInterval(nextSlide, 3000);
 window.addEventListener('scroll', navbarOpacity);
+
+
+const sendbtn = document.queryselector("button");
+const etter = document.queryselector(".etter");
+const widget = document.queryselector(".star_widget");
+const editBtn = document.queryselector(".edit");
+sendbtn.onclick = ()=>{
+    widget.style.display = "none";
+    etter.style.display = "block";
+    editBtn.onclick = ()=>{
+        widget.style.display = "block";
+        etter.style.display = "none";
+        return false;
+    }
+}
