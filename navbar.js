09cadad406ebc29da8616aeb4369880cedc088fc
Adele Strysse <axstryss@stud.ntnu.no>
2023-10-25 21:36:51 +0200

    navbar js

--- navbar.js
+++ navbar.js
@@ -2,6 +2,20 @@ class Header extends HTMLElement {
     constructor() {
         super();
         this.innerHTML = `
+        <style>
+            .navbar{
+                background-color: #FDF0D5;
+                position: fixed;
+                display: grid;
+                grid-template-columns: repeat(auto-fit, 1fr);
+                width: 100%;
+                text-align: center;
+                box-shadow: 0 0 50px grey;
+                top: 0;
+                z-index: 1;
+                transition: 0.5s ease-in-out;
+            }
+        </style>
         <nav>
             <div class="heleLogo">
                 <img id="logo" src="img/logo_1.png" alt="LogoMunchMeter">
