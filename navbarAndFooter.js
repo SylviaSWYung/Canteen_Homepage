447d4e413d081564f60020e4c03cbd731be4e7c9
Adele Strysse <axstryss@stud.ntnu.no>
2023-10-26 11:07:06 +0200

    kolblinger mellom sidene

--- navbarAndFooter.js
+++ navbarAndFooter.js
@@ -85,23 +85,23 @@ class Header extends HTMLElement {
         <nav class="navbar">
             <div class="heleLogo">
                 <img id="logo" src="img/logo_1.png" alt="LogoMunchMeter">
-                <h1><a href="#" style="color: inherit; text-decoration: none;">MUNCHMETER</a></h1>
+                <h1><a href="homepage.html" style="color: inherit; text-decoration: none;">MUNCHMETER</a></h1>
                 </div>
                 <div class="oversikt">
                         <div class="dropdown">                                                  <!--Dropwdown for cafe-->
                             <button class="dropbtn" id="kafe">Kafé</button>
                             <div class="dropdown_innhold">
-                                <a href="#">SITO Realfag</a>
+                                <a href="sitorealfag.html">SITO Realfag</a>
                                 <a href="#">SITO Hangarn</a>
-                                <a href="#">SITO Elektro</a>
+                                <a href="elbygget.html">SITO Elektro</a>
                             </div>
                         </div>            
                         <div class="dropdown">                                                  <!--Dropwdown for kantine-->
                             <button class="dropbtn" id="kantine">Kantine</button>
                             <div class="dropdown_innhold">
-                                <a href="#">Element</a>
-                                <a href="#">Hangarn</a>
-                                <a href="#">Kjelhuset</a>
+                                <a href="element.html">Element</a>
+                                <a href="hangarn.html">Hangarn</a>
+                                <a href="kjelhuset.html">Kjelhuset</a>
                             </div>
                         </div>
                     <a id="omOss" href="#">Om oss</a>
